158232ab1ca5cb75eb81074f77dbadb433da8871
DAR <dar@browndeertechnology.com>
2016-07-14 03:23:48 +0000

    small corrections to copyright notices

--- cannon/cannon_host.c
+++ cannon/cannon_host.c
@@ -1,8 +1,7 @@
 /* cannon_host.c
  *
- * Copyright (c) 2015, James A. Ross
- * Copyright (c) 2015, Brown Deer Technology, LLC.
- * All rights reserved.
+ * Copyright (c) 2015 James A. Ross.  All rights reserved.
+ * Copyright (c) 2015 Brown Deer Technology, LLC.  All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
--- cannon/cannon_tfunc.c
+++ cannon/cannon_tfunc.c
@@ -1,8 +1,7 @@
 /* mpi_tfunc.c
  *
- * Copyright (c) 2016, Brown Deer Technology, LLC.
- * Copyright (c) 2015, James A. Ross
- * All rights reserved.
+ * Copyright (c) 2016 Brown Deer Technology, LLC.  All rights reserved.
+ * Copyright (c) 2015 James A. Ross.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
